52e04c7d5f6af853140c06c65cc42dfb806128c7
clookin <sebast305@hotmail.com>
2024-08-27 12:29:14 -0500

    se agregaron modales y media querys

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
 let inputTexto = document.getElementById('input-encriptador'); // Obtiene el elemento input
+const mensajeDiv = document.getElementById('mensaje');
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const cerrarBtn = document.getElementById('cerrar-instrucciones');
+  const instruccionesDiv = document.getElementById('instrucciones');
+
+  cerrarBtn.addEventListener('click', () => {
+    instruccionesDiv.style.display = 'none';
+  });
+});
 
 function mostrarMensaje(texto, tipo) {
-  mensajeDiv.textContent = texto;
-  mensajeDiv.className = `mensaje ${tipo}`;
-  mensajeDiv.style.display = 'block';
+  mensajeDiv.textContent = texto; // Contenido del texto
+  mensajeDiv.className = `mensaje ${tipo}`; // Modificador de estilos segun el segundo argumento Success o error
+  mensajeDiv.style.display = 'block'; //modificador de apariencia
 }
 
 function validarTexto(texto){
-  const validacion = /^[a-zA-Z\s]*$/; //Expresion regular para validar caracteres especiales
+  const validacion = /^[a-z-\s]*$/; //Expresion regular para validar caracteres especiales
   return validacion.test(texto)
 }
 
 function encriptar() {
-  const texto = inputTexto.value.trim(); // Obtiene y recorta el texto del input
+  const texto = inputTexto.value.trim().toLowerCase(); // Obtiene y recorta el texto del input tambien lo hace minuscula
   if (texto !== "") {
     if (validarTexto(texto)){
       const textoSeparado = separadorTexto(texto); // Separa el texto en caracteres
       inputTexto.value = codificadortexto(textoSeparado); // Encripta el texto y lo asigna al input
       mostrarMensaje('Texto encriptado correctamente.', 'success');
+      setTimeout(() => {
+        mensajeDiv.style.display = 'none';
+      }, 3000);
     } else {
-      mostrarMensaje('El texto contiene caracteres no permitidos.', 'error');
-      
+      mostrarMensaje('El texto contiene caracteres no permitidos: Mayusculas y caracteres especiales.', 'error');
+      setTimeout(() => {
+        mensajeDiv.style.display = 'none';
+      }, 4000);
     }
   } else {
-    console.log('ingresa un texto'); // Mensaje si el input está vacío
+    mostrarMensaje('Debes ingresar un texto para encriptar.', 'error') // Mensaje si el input está vacío
+    setTimeout(() => {
+      mensajeDiv.style.display = 'none';
+    }, 3000);
   }
 }
 
@@ -45,33 +63,41 @@ function codificadortexto(textoSeparado) {
     }
   }
   textoEncriptado = textoCodificado.join(''); // Une el array en una cadena
-  console.log(textoEncriptado); // Muestra el texto encriptado en la consola
   return textoEncriptado;
 }
 
 function desencriptar() {
-  const texto = inputTexto.value.trim(); // Obtiene y recorta el texto del input
+  const texto = inputTexto.value.trim().toLowerCase(); // Obtiene y recorta el texto del input
   if (texto !== "") {
     const textoDescencriptado = decodificadorTexto(texto); // Desencripta el texto
     inputTexto.value = textoDescencriptado; // Asigna el texto desencriptado al input
     return textoDescencriptado;
   }
 }
-
 function decodificadorTexto(texto) {
   let codigos = { ai: "a", enter: "e", imes: "i", ober: "o", ufat: "u" };
   let textoDescencriptado = texto;
   for (let codigo in codigos) {
     textoDescencriptado = textoDescencriptado.replaceAll(codigo, codigos[codigo]); // Reemplaza códigos por vocales
   }
+  mostrarMensaje('Texto desencriptado correctamente.','success');
+  setTimeout(() => {
+    mensajeDiv.style.display = 'none';
+  }, 3000);
   return textoDescencriptado;
 }
 
 function copiarTexto (){
   const texto = inputTexto.value.trim();
-  navigator.clipboard.writeText(texto).then(function() {
-    console.log('Texto copiado al portapapeles');
+  navigator.clipboard.writeText(texto).then(function() { //Funcion nativa para copiar texto del textarea en el portapapeles
+    mostrarMensaje('Texto copiado en el portapapeles.','success');
+    setTimeout(() => {
+      mensajeDiv.style.display = 'none';
+    }, 3000);
 }).catch(function(err) {
-    console.error('Error al copiar texto: ', err);
+    mostrarMensaje('Error al copiar texto: ', 'error');
+    setTimeout(() => {
+      mensajeDiv.style.display = 'none';
+    }, 3000);
 });
 }
